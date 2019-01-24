20e9e2a71fbc22a3ad362d3af149c23bfa764bb1
Sophie Engle <sjengle@cs.usfca.edu>
2019-01-23 16:47:43 -0800

    Adding file IO and exception demos.

---
+++ Files and Exceptions/templates/DirectoryStreamDemo.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.nio.file.Path;
+
+@SuppressWarnings({"javadoc", "unused"})
+public class DirectoryStreamDemo {
+
+	private static void traverse(String prefix, Path path) throws IOException {
+		// TODO
+	}
+
+	public static void traverse(Path directory) throws IOException {
+		// TODO
+	}
+
+	public static void main(String[] args) throws IOException {
+		// TODO
+		throw new UnsupportedOperationException("Not yet implemented.");
+	}
+
+}
