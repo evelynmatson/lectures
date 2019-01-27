28c559b3bc0d970eb948a465bf217261de39e21a
Sophie Engle <sjengle@cs.usfca.edu>
2019-01-27 14:29:56 -0800

    Updated data structure examples.

---
+++ Data Structures/templates/NestedMapDemo.java
@@ -0,0 +1,11 @@
+@SuppressWarnings("javadoc")
+public class NestedMapDemo {
+
+	public static final String text = "time flies like an arrow fruit flies like a banana";
+
+	public static void main(String[] args) {
+
+		// TODO
+
+	}
+}
