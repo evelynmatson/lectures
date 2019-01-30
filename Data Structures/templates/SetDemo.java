16b1ce454070c3f101639f6b3aa88115568853f2
Sophie Engle <sjengle@cs.usfca.edu>
2019-01-30 14:14:22 -0800

    Adding set/mutability demo.

---
+++ Data Structures/templates/SetDemo.java
@@ -0,0 +1,19 @@
+import java.util.TreeSet;
+
+@SuppressWarnings("javadoc")
+public class SetDemo {
+
+	public static void main(String[] args) {
+
+		TreeSet<StringBuilder> set = new TreeSet<>();
+
+		set.add(new StringBuilder("banana"));
+		set.add(new StringBuilder("cherry"));
+		set.add(new StringBuilder("apple"));
+
+		System.out.println(set);
+
+		// TODO
+	}
+
+}
